3b7e6f2a2c99ce0416e09ee3bf36b5025be8089d
AugmentedDuck <anders.m.matzen@gmail.com>
2022-11-23 13:00:38 +0100

    Made it calculate the difference

--- src/add.js
+++ src/add.js
@@ -4,18 +4,42 @@ var form = document.getElementById("form-add-item");
     var completeList = document.getElementById("food-list");
     var foodName = document.getElementById("input-add-item-name").value
     var foodDate = document.getElementById("input-add-item-date").value
+    var dateYYYY = ""
+    var dateMM = ""
+    var dateDD = ""
+
+    for (let i = 0; i < foodDate.length; i++) {
+      if (i < 4){
+        dateYYYY += foodDate[i];
+        console.log(foodDate[i])
+      } else if (4 < i && i < 7) {
+        dateMM += foodDate[i];
+        console.log(foodDate[i])
+      } else if (7 < i) {
+        dateDD += foodDate[i];
+        console.log(foodDate[i])
+      }
+    }
 
     var today = new Date();
     var dd = today.getDate();
-    var mm = today.getMonth() + 1; //January is 0!
+    var mm = today.getMonth() + 1;
     var yyyy = today.getFullYear();
+    console.log(dateYYYY + " " + dateMM + " " + dateDD + " | " + yyyy + " " + mm + " " + dd)
+    
+    var difference = 0
 
-    console.log(foodDate + " " + yyyy + "-" + mm + "-" + dd)
-     
+    difference += (dateDD - dd) + ((dateMM - mm) * 30) + ((dateYYYY - yyyy) * 365)
+
     completeList.innerHTML += `<div class="food-row">
         <button class="btn delete-btn food-delete food-column" role="button">X</button>
         <span class="food-item food-column">` + foodName + `</span>
-        <span class="food-time food-column">` + foodDate + `</span>
+        <span class="food-time food-column">` + difference + " dage" + `</span>
       </div>`
+    
+    if (difference < 5) {
+      alert("Advarsel\n" + foodName + " udløber om " + difference + " dage");
+    }
   } 
+  
   form.addEventListener('submit', handleForm);
